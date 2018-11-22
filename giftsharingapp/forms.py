7c9122dcd0f376416462e5d56b27e98bb9ca4cf3
Yulia Shea <yulia.shea@gmail.com>
2018-11-22 12:40:04 -0500

    make some fields in form not required

--- giftsharingapp/forms.py
+++ giftsharingapp/forms.py
@@ -12,21 +12,25 @@ class CreateGiftForm(forms.Form):
         label="Gift Description",
         max_length=2000,
         help_text="Describe your desired gift",
-        widget=forms.Textarea(attrs={'class': 'form-control'})
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+        required=False
     )
     link = forms.URLField(
         max_length=2000,
         label="Link to the gift",
         widget=forms.URLInput(attrs={'class': 'form-control'}),
+        required=False
         )
     price = forms.DecimalField(
         max_digits=6,
         decimal_places=2,
         widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        required=False
     )
     desirability_rank = forms.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)],
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        required=False
     )
     active_til = forms.DateField(
         help_text="Provide a date if you want your gift wish to expire after a particular time",
