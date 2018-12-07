00dc8d8c3731f1c61024f3fa954deb7c4cd4f59e
Yulia Shea <yulia.shea@gmail.com>
2018-12-06 21:15:28 -0500

    adding friendship

--- giftsharingapp/admin.py
+++ giftsharingapp/admin.py
@@ -1,7 +1,8 @@
+from giftsharingapp.models import Gift, GifterGroup, UserInfo, Friendship
 from django.contrib import admin
-from giftsharingapp.models import Gift, GifterGroup, UserInfo
 
 # Register your models here.
 admin.site.register(Gift)
 admin.site.register(GifterGroup)
 admin.site.register(UserInfo)
+admin.site.register(Friendship)
